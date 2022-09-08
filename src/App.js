37c7ee3173c43ecab3ef3b33d79e8b51cda7b20e
Sofia Dinatolo <92044969+cockatiella@users.noreply.github.com>
2022-09-08 13:07:45 +0200

    added some blur

--- src/App.js
+++ src/App.js
@@ -12,6 +12,30 @@ function App() {
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
       <div className="blur" style={{ bottom: "6%", right: "2rem" }}></div>
+      <div className="blur" style={{ bottom: "6%", left: "2rem" }}></div>
+      <div className="blur" style={{ bottom: "66%", right: "1rem" }}></div>
+      <div className="blur" style={{ bottom: "1%", right: "2rem" }}></div>
+      <div className="blur" style={{ bottom: "1%", left: "2rem" }}></div>
+      <div className="blur" style={{ bottom: "66%", left: "20" }}></div>
+      <div className="blur" style={{ bottom: "88%", left: "10px" }}></div>
+      <div className="blur" style={{ bottom: "8%", right: "2rem" }}></div>
+      <div className="blur" style={{ bottom: "50%", right: "20rem" }}></div>
+      <div className="blur" style={{ top: "20%", right: "2rem" }}></div>
+      <div className="blur" style={{ top: "120%", right: "2rem" }}></div>
+      <div className="blur" style={{ top: "180%", right: "2rem" }}></div>
+      <div className="blur" style={{ top: "220%", right: "2rem" }}></div>
+      <div className="blur" style={{ top: "250%", right: "2rem" }}></div>
+      <div className="blur" style={{ top: "300%", right: "2rem" }}></div>
+      <div className="blur" style={{ top: "320%", right: "2rem" }}></div>
+      <div className="blur" style={{ top: "400%", right: "2rem" }}></div>
+      <div className="blur" style={{ top: "120%", left: "1.5rem" }}></div>
+      <div className="blur" style={{ top: "190%", left: "2rem" }}></div>
+      <div className="blur" style={{ top: "180%", left: "1rem" }}></div>
+      <div className="blur" style={{ top: "250%", left: "0.5rem" }}></div>
+      <div className="blur" style={{ top: "300%", left: "3rem" }}></div>
+      
+     
+      
 
       <Routes>
         <Route path="/" element={<Home />} />
